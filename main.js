523f69b23113f9b17ed109f2f827462741864a42
Abdullah Al Fuwad <fuwad25web@gmail.com>
2025-08-06 01:58:58 +0600

    fixed sign up login page

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // Common password toggle function for both login and signup
 function togglePassword(inputId, eyeIcon) {
   const passwordInput = document.getElementById(inputId);
+  const iconElement = eyeIcon.querySelector('i');
 
   if (passwordInput.type === "password") {
     passwordInput.type = "text";
-    eyeIcon.classList.remove("fa-eye");
-    eyeIcon.classList.add("fa-eye-slash");
+    iconElement.classList.remove("fa-eye");
+    iconElement.classList.add("fa-eye-slash");
   } else {
     passwordInput.type = "password";
-    eyeIcon.classList.remove("fa-eye-slash");
-    eyeIcon.classList.add("fa-eye");
+    iconElement.classList.remove("fa-eye-slash");
+    iconElement.classList.add("fa-eye");
   }
 }
 
